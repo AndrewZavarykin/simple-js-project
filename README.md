fa6803a02c0c3a573577f57a9b6f87f39f28f774
andrew zavarykin <andrewzavarykin@gmail.com>
2026-05-06 00:51:57 -0700

    настройка webpack

--- README.md
+++ README.md
@@ -23,3 +23,7 @@
 `npm install --save-dev webpack webpack-cli` - установка Webpack
 
 `npm install --save-dev css-loader style-loader` - добавление поддержки CSS в Webpack
+
+`npm install --save-dev html-webpack-plugin` - установка плагина для создания HTML в dist
+
+`npm install --save-dev webpack-dev-server` - установка сервера разработки Webpack
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { template } from "@babel/core";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,6 +13,7 @@ export default {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin(template("index.html"))],
   module: {
     rules: [
       {
@@ -18,5 +21,12 @@ export default {
         use: ["style-loader", "css-loader"],
       },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
   },
 };
